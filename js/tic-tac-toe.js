3b2ea3ad7f7010f61b64ed698ae8bca2a68cb3d3
Garrett Maitland <gmaitlan18@gmail.com>
2025-07-03 10:59:39 -0400

    feat: implement Player constructer and Players module to keep track of the current player

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -23,7 +23,51 @@ let Gameboard = (function () {
   return {setPiece, getPiece};
 })();
 
+function Player(name, piece) {
+  let getName = () => {
+    return name;
+  };
+
+  let getPiece = () => {
+    return piece;
+  };
+
+  return {getName, getPiece};
+}
+
+const Players = (function () {
+  let playerIndex = -1;
+  let players = [];
+
+  let addPlayer = (player) => {
+    players.push(player);
+  };
+
+  let currentPlayer = () => {
+    if (playerIndex === -1) {
+      if (players.length === 0) {
+        return null;
+      } else {
+        playerIndex = 0;
+      }
+    }
+    return players[playerIndex];
+  };
+
+  let nextPlayer = () => {
+    let numOfPlayers = players.length;
+    if (numOfPlayers === 0) {
+      return null;
+    }
+    playerIndex = (playerIndex + 1) % numOfPlayers;
+    return players[playerIndex];
+  };
+
+  return {addPlayer, currentPlayer, nextPlayer};
+})();
+
 function refreshView() {
+  console.log(`\n\n${Players.currentPlayer().getName()}'s turn\n`)
   for (let i = 0; i < NUM_OF_COLS; i++) {
     let row = "";
     for (let j = 0; j < NUM_OF_ROWS; j++) {
